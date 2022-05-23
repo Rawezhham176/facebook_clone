3433683390d9714365df204a059f3a45e041ea90
Rawezh Hama <rawezhhame@gmail.com>
2022-05-23 02:08:18 +0200

    first commit

--- next.config.js
+++ next.config.js
@@ -1,6 +1,19 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  images: {
+    domains: [
+      "upload.wikimedia.org",
+      "platform-lookaside.fbsbx.com",
+      "profile-images.xing.com",
+      "firebasestorage.googleapis.com",
+      "i.pinimg.com",
+      "images.pexels.com",
+      "images.squarespace-cdn.com",
+      "i1.adis.ws",
+      "epicphoto.de",
+    ],
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
